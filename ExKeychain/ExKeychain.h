2b106a4c95f33f4103d02028d22377fede0ca64c
Rudolph van Graan <rvg@me.com>
2015-08-25 15:44:54 +0100

    Initial Import

---
+++ ExKeychain/ExKeychain.h
@@ -0,0 +1,19 @@
+//
+//  ExKeychain.h
+//  ExKeychain
+//
+//  Created by Rudolph van Graan on 25/08/2015.
+//  Copyright (c) 2015 Curoo Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExKeychain.
+FOUNDATION_EXPORT double ExKeychainVersionNumber;
+
+//! Project version string for ExKeychain.
+FOUNDATION_EXPORT const unsigned char ExKeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExKeychain/PublicHeader.h>
+
+
